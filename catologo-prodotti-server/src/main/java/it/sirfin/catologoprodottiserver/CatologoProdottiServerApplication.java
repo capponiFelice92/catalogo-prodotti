d56ba1f255920853c4838487858ab6aeed6ef4fa
Matteo Moretti <78532093+matteo2025@users.noreply.github.com>
2021-02-15 13:47:40 +0100

    aggiunte annotazioni classe main

--- catologo-prodotti-server/src/main/java/it/sirfin/catologoprodottiserver/CatologoProdottiServerApplication.java
+++ catologo-prodotti-server/src/main/java/it/sirfin/catologoprodottiserver/CatologoProdottiServerApplication.java
@@ -2,8 +2,14 @@ package it.sirfin.catologoprodottiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"it.sirfin.catalogoprodottiserver"})
+@EnableJpaRepositories(basePackages= {"it.sirfin.catalogoprodottiserver.repository"})
+@EntityScan(basePackages= {"it.sirfin.catalogoprodottiserver.model"})
 public class CatologoProdottiServerApplication {
 
 	public static void main(String[] args) {
