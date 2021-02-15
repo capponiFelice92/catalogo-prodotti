76b20257008baa0f66b9452833d9627994745bf8
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-02-15 17:21:56 +0100

    repository ed entity prodotto

--- catologo-prodotti-server/src/main/java/it/sirfin/catologoprodottiserver/model/Prodotto.java
+++ catologo-prodotti-server/src/main/java/it/sirfin/catologoprodottiserver/model/Prodotto.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package it.sirfin.catologoprodottiserver.model;
 
-/**
- *
- * @author Hp Desktop
- */
-public class Prodotto {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Prodotto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column
+    private String descrizione;
+    @Column
+    private double prezzo;
+
+    public Prodotto() {
+    }
+
+    public Prodotto(String descrizione, double prezzo) {
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "Prodotto{" + "id=" + id + ", descrizione=" + descrizione + ", prezzo=" + prezzo + '}';
+    }
+
+
+    
     
 }
--- catologo-prodotti-server/src/main/java/it/sirfin/catologoprodottiserver/repository/ProdottoRepository.java
+++ catologo-prodotti-server/src/main/java/it/sirfin/catologoprodottiserver/repository/ProdottoRepository.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package it.sirfin.catologoprodottiserver.repository;
 
-/**
- *
- * @author Hp Desktop
- */
-public class ProdottoRepository {
-    
+import it.sirfin.catologoprodottiserver.model.Prodotto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdottoRepository extends JpaRepository<Prodotto, Long>{
 }
+
