6a321962d1848b83a761b835a745b4097f0aa9f3
capponiFelice92 <78529321+capponiFelice92@users.noreply.github.com>
2021-02-15 13:43:41 +0100

    creato server vuoto

---
+++ catologo-prodotti-server/src/main/java/it/sirfin/catologoprodottiserver/CatologoProdottiServerApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.catologoprodottiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatologoProdottiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatologoProdottiServerApplication.class, args);
+	}
+
+}
---
+++ catologo-prodotti-server/src/test/java/it/sirfin/catologoprodottiserver/CatologoProdottiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.catologoprodottiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatologoProdottiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
